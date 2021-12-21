656266f6e7b131246039a54e686789376a69afc1
Wahyu Rahmana <tn.wahyurahmana@gmail.com>
2021-12-21 21:26:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 let password = 'I Love Javascript'
 let censoredWord = 'love'
 let newPassword = ""; 
-let point = 0;
 
 
 for (let i= 0;  i < password.length; i++) {
   let inCorrect = 0
   if (password[i] !== " ") {
     for(let j = 0; j < censoredWord.length; j++){
-      point++
       if (password[i].toLowerCase() !== censoredWord[j]) {
         inCorrect += 1;
       }
@@ -19,6 +17,5 @@ for (let i= 0;  i < password.length; i++) {
       newPassword += "*"
     }
   }
-  point +=1;
 }
 console.log(newPassword, point)
